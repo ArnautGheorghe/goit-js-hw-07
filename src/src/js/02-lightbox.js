3c9ae8468e6be6535f807afc51250b42106ab29d
ArnautGheorghe <agigi94@yahoo.com>
2024-01-09 17:43:03 +0200

    exercitiil 2

--- src/src/js/02-lightbox.js
+++ src/src/js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("gallery__item");
+  listItemEl.innerHTML = `<a class='gallery__link' href='${item.original}'>
+        <img class='gallery__image' 
+        src='${item.preview}' 
+        data-source='${item.original}' 
+        alt='${item.description}'/>
+    </a>`;
+    listEl.append(listItemEl);
+    
+});
+
+let captionPosition= "outside"
+let gallery = new SimpleLightbox(".gallery a", { captionPosition });
+gallery.on("show.simplelightbox");
+
